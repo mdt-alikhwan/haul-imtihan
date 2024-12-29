3222c965fa697237d80c141e89b8af1de23f32f2
mdt-alikhwan <store.bharatainternasional@gmail.com>
2024-12-30 05:09:40 +0700

    Update haflah.js

--- haflah.js
+++ haflah.js
@@ -1,3 +1,14 @@
+window.onload = function() {
+    // Request fullscreen mode
+    if (document.documentElement.requestFullscreen) {
+        document.documentElement.requestFullscreen();
+    } else if (document.documentElement.webkitRequestFullscreen) {
+        document.documentElement.webkitRequestFullscreen();
+    } else if (document.documentElement.msRequestFullscreen) {
+        document.documentElement.msRequestFullscreen();
+    }
+};
+
 document.querySelectorAll('.nav-indicator .btn').forEach(button => {
     button.addEventListener('click', function () {
         const targetId = this.getAttribute('data-target');
